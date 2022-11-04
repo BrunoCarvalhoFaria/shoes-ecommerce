b459c11a8c7657b070d6f517214f08d7cf234d6b
Bruno Faria <devbrunofaria@gmail.com>
2022-11-04 15:34:04 -0300

    melhorando layout

--- src/components/shared/header/index.js
+++ src/components/shared/header/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./style.css"
+import { useState } from "react";
 
 
 
@@ -8,31 +9,68 @@ const Header = () => {
   let navigate = useNavigate();
 
   const goToLogin = () => {
+    setActive('')
     navigate('/login');
   }
   const goToRegister = () => {
+    setActive('')
     navigate('/register');
   }
   const goToStores = () => {
+    setActive('')
     navigate('/stores');
   }
 
   const goToHome = () => {
+    setActive('')
     navigate('/');
   }
 
+  const goToCart = () => {
+    setActive('')
+  }
+
+  const [active, setActive] = useState('') 
+
+  const changeActive = () => {
+    if (active === ''){
+      setActive("active")
+    }else {
+      setActive('')
+    }
+  }
+
   return (
-  <div className='header'>
-    <div className='logo'>
-      <img onClick= {goToHome} src='../../../static/images/logo.png' alt='Logo Dev Bruno Shoes'/>
-    </div>
-    <div className='menu'>
-      <button onClick={goToStores}>Stores</button>
-      <button onClick={goToRegister}>Register</button>
-      <button onClick={goToLogin}>Login</button>
-      <button className='carrinho'><img src="../../../static/images/carrinho.png" alt="carrinho"/></button>
-    </div>
-  </div>
+    <header>
+      <nav className="navbar">       
+        <div className="nav-brading">          
+          <img src="../../../static/images/logo.png" alt="logo"/>
+          <span>Dev. Bruno</span>
+        </div>
+        <ul className={`nav-menu ${active}`}>
+          <li className="nav-item">             
+            <span className="nav-link" onClick={goToHome}>Home</span>            
+          </li>
+          <li className="nav-item">          
+            <span className="nav-link" onClick={goToStores}>Stores</span>            
+          </li>
+          <li className="nav-item">          
+            <span className="nav-link" onClick={goToRegister}>Register</span>
+          </li>
+          <li className="nav-item">          
+            <span className="nav-link" onClick={goToLogin}>Login</span>
+          </li>
+          <li className="nav-item">      
+            <span className="nav-link"><img onClick={goToCart} className='cart' src="../../../static/images/carrinho.png" alt="cart"/></span>            
+          </li>
+        </ul>
+        <div className={`hamburger ${active}`} onClick = {changeActive}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>         
+        </div>
+      </nav>
+    </header>
   )
 }
 
